8e3091ce2378cc730c9b7d751a75e367d2e696c7
Drew Pepin <drewpepin1995@gmail.com>
2019-09-07 20:35:12 -0400

    Finishing doWhatItSays function/call

--- liri.js
+++ liri.js
@@ -9,61 +9,71 @@ let axios = require('axios');
 let liriReturn = process.argv[2];
 var moment = require('moment');
 var fs = require('fs');
+let inputParameter = process.argv.slice(3, process.argv.length).join(" ");
 
-switch (liriReturn) {
-    case "spotify-this-song":
-        spotifyThis();
-        break;
+userInput(liriReturn, inputParameter);
 
-    case "concert-this":
-        concertThis();
-        break;
+function userInput(liriReturn, inputParameter) {
+    switch (liriReturn) {
+        case "spotify-this-song":
+            spotifyThis(inputParameter);
+            break;
 
-    case "movie-this":
-        moviesThis();
-        break;
+        case "concert-this":
+            concertThis(inputParameter);
+            break;
 
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
+        case "movie-this":
+            moviesThis(inputParameter);
+            break;
 
-    default: console.log("\nType any of these commands after 'node liri.js' : " +
-        "\nspotify-this-song 'any song title here'" +
-        "\nmovie-this 'any movie title here'" +
-        "\nconcert-this 'artist/band name here'" +
-        "\ndo-what-it-says");
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
 
+        default: console.log("\nType any of these commands after 'node liri.js' : " +
+            "\nspotify-this-song 'any song title here'" +
+            "\nmovie-this 'any movie title here'" +
+            "\nconcert-this 'artist/band name here'" +
+            "\ndo-what-it-says");
+
+    };
 };
 
-function spotifyThis(mediaName) {
-    let trackName = process.argv.slice(3, process.argv.length).join(" ");
+function spotifyThis(inputParameter) {
+
+    if (!inputParameter) {
+        inputParameter === 'The Sign';
+    }
+
     spotify.search({
         type: 'track',
-        query: trackName,
+        query: inputParameter,
         limit: 5
-    })
-        .then(function (response) {
-            response.tracks.items.forEach(function (number) {
-                console.log(" ");
-                console.log("Artist(s): " + number.album.artists[0].name);
-                console.log("Song Name: " + number.name);
-                console.log("Spotify Preview: " + number.preview_url);
-                console.log("Album: " + number.album.name);
-                console.log("-----------------------------------------------------------------------------------------")
+    }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+        console.log(" ");
+        console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Spotify Preview: " + data.tracks.items[0].href);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("-----------------------------------------------------------------------------------------")
 
-            })
-        })
-        .catch(function (error) {
-            console.log("Error occurred: " + error);
-        });
+    });
+
+
+
+
+
+
 };
 
-function moviesThis() {
-    let movieTitle = process.argv.slice(3, process.argv.length).join(" ");
-    let queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movieTitle + "";
+function moviesThis(inputParameter) {
+    let queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + inputParameter + "";
 
-    if (movieTitle === "") {
-        let movieTitle = process.argv.slice(3, process.argv.length).join(" ");
+    if (inputParameter === "") {
         let queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=Mr.+Nobody";
 
         axios.get(queryURL)
@@ -106,38 +116,35 @@ function moviesThis() {
 
 };
 
-function concertThis() {
-
-    let artist = process.argv.slice(3, process.argv.length).join(" ");
-    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function concertThis(inputParameter) {
+    let queryURL = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
 
     axios.get(queryURL)
-        .then(function(response){
-           for (var i=0; i<response.data.length; i++) {
-               console.log("Concert Time: " + moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format('MM/DD/YYYY'));
-               console.log("Concert Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-               console.log("Concert Venue: " + response.data[i].venue.name);
-               console.log("------------------------------------------------")
-           }
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Concert Time: " + moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format('MM/DD/YYYY'));
+                console.log("Concert Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+                console.log("Concert Venue: " + response.data[i].venue.name);
+                console.log("------------------------------------------------")
+            }
         })
-        .catch(function(error){
+        .catch(function (error) {
             console.log(error);
         })
 
 };
 
 function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", function(error, data){
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
         } else {
 
             let randomData = data.split(",");
-            let mediaName = randomData[1].replace(/^"(.+(?="$))"$/, '$1');
-            spotifyThis(mediaName);
+            userInput(randomData[0], randomData[1]);
 
-            
-            
+
+
         }
     })
 }
